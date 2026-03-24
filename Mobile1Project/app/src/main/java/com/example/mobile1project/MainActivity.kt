cffae74abe99fe1a26109c64ceec24d0aa9ca260
Cristhian Loya <15186@ulsachihuahua.edu.mx>
2026-03-23 19:15:25 -0600

    revisar si se puede hacer cambios

--- Mobile1Project/app/src/main/java/com/example/mobile1project/MainActivity.kt
+++ Mobile1Project/app/src/main/java/com/example/mobile1project/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
             Mobile1ProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Sebastian 14970 y Cristhian 15186",
+                        name = "Sebastian 14970 y Cristhian 15186 HHHHHHHHHHHHHHHHHHHHHHHH",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
