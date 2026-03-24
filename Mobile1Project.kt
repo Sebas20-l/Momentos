2909cac8a12577bb90f3df613e2c1f774f819948
Sebastián <sebastiannevarezr@gmail.com>
2026-03-23 18:42:05 -0600

    codigo

--- Mobile1Project.kt
+++ Mobile1Project.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Mobile1ProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Sebastian y Cristhian",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello Sebastian y Cristhian",
+        text = "Hello, $name",
         modifier = modifier
     )
 }
