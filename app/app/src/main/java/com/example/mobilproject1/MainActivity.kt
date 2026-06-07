a8598422a418917b4d8c29f4dfa81813507abe47
Cristhian Loya <15186@ulsachihuahua.edu.mx>
2026-06-07 14:31:55 -0600

    n

--- app/app/src/main/java/com/example/mobilproject1/MainActivity.kt
+++ app/app/src/main/java/com/example/mobilproject1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Mobilproject1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Sebastian y Cristhiannnnnnnnnn",
+                        name = "Sebastiann y Cristhiannnnnnnnnn",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
