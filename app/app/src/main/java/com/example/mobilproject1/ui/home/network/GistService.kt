05ec1fca02b2e61640dbd32a35ec04f623a2b420
Cristhian Loya <15186@ulsachihuahua.edu.mx>
2026-06-08 00:22:39 -0600

    feat: add GistService and GistClient with Retrofit

--- app/app/src/main/java/com/example/mobilproject1/ui/home/network/GistService.kt
+++ app/app/src/main/java/com/example/mobilproject1/ui/home/network/GistService.kt
@@ -1,27 +1,19 @@
 package com.example.mobilproject1.ui.home.network
 
+import com.example.mobilproject1.ui.home.model.MemoriesResponse
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-data class GistFile(
-    val filename: String,
-    val content: String
-)
-
-data class GistResponse(
-    val files: Map<String, GistFile>
-)
-
 interface GistService {
-    @GET("gists/79da75bb443de39d6368d02cf13e92fb")
-    suspend fun getGist(): GistResponse
+    @GET("Cristhian-LR/bce2d0b164d66e06a92b0f12973f8578/raw/30abc09bf0f242ad4355b57759e7714d4bbcef01/memories.json")
+    suspend fun getMemories(): MemoriesResponse
 }
 
 object GistClient {
     val service: GistService by lazy {
         Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
+            .baseUrl("https://gist.githubusercontent.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(GistService::class.java)
